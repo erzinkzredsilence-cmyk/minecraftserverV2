571e16bde3d3a6fbbdb0433b82168787e5201c1a
erzinkzredsilence-cmyk <erzinkzredsilence@gmail.com>
2026-02-28 17:49:38 +0700

    Initialize bot code for bedrock server

---
+++ bot.js
@@ -0,0 +1,22 @@
+const bedrock = require('bedrock-protocol');
+const io = require('socket.io-client');
+
+const RENDER_URL = 'https://minecraftvoiceserver.onrender.com';
+const socket = io(RENDER_URL);
+
+// Join the server as a bot
+const client = bedrock.createClient({
+    host: 'zeingaymakmak.mc.in.th', // From your panel
+    port: 27677,            // Default Bedrock port
+    username: 'Zeincodgay',
+    offline: true           // Set to false if server needs Xbox login
+});
+
+client.on('spawn', () => {
+    console.log('Zeincodgay joined Bedrock server!');
+
+    // Bedrock tracking is different, but this joins the game
+    setInterval(() => {
+        // Logic to track players via 'client' packets goes here
+    }, 1000);
+});
